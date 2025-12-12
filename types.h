62e13c3c46ffa012083abb471a921c81b90e7943
cata-884 <catalin.rusu884@gmail.com>
2025-12-12 13:57:14 +0200

    structura de baza pentru clase si functii

--- types.h
+++ types.h
@@ -1,1 +1,25 @@
+#ifndef TYPES_H
+#define TYPES_H
 
+#include <string>
+
+enum TipBaza {
+    TYPE_INT,
+    TYPE_FLOAT,
+    TYPE_STRING,
+    TYPE_BOOL,
+    TYPE_VOID,
+    TYPE_CLASS,   
+    TYPE_UNKNOWN
+};
+
+
+struct TypeInfo {
+    TipBaza type;
+    std::string className; //folosit cand declaram o clasa
+
+    TypeInfo(TipBaza t) : type(t), className("") {}
+    TypeInfo(std::string name) : type(TYPE_CLASS), className(name) {}
+};
+
+#endif
