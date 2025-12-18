d9febc427c6d312d7ea51d88af4ee95eb86a4cf3
cata-884 <catalin.rusu884@gmail.com>
2025-12-18 13:06:47 +0200

    comentarii la ast.h

--- ast.h
+++ ast.h
@@ -8,29 +8,43 @@
 
 using namespace std;
 
+//clasa de baza pentru toate nodurile din arbore(abstracta)
 class ASTNode {
 public:
+    //destructor virtual necesar pentru a sterge corect obiectele derivate
     virtual ~ASTNode() {}
+    
+    //functie de afisare recursiva
+    //level=nivelul de indentare(0 pentru radacina)
     virtual void print(int level = 0) {
+        //afisam spatii pentru a simula structura arborescenta
         for(int i=0; i<level; i++) cout << "  ";
     }
 };
 
+//radacina intregului program
 class ProgramNode : public ASTNode {
 public:
-    vector<ASTNode*> globals;
-    ASTNode* mainBlock;
+    vector<ASTNode*> globals; //vector pentru variabile globale/functii/clase
+    ASTNode* mainBlock;       //pointer catre blocul principal main()
+    
     void print(int level = 0) override {
         ASTNode::print(level); cout << "PROGRAM ROOT" << endl;
+        //iteram prin globale si le afisam
         for (auto n : globals) n->print(level + 1);
+        //daca exista main,il afisam
         if (mainBlock) mainBlock->print(level + 1);
     }
 };
 
+//bloc de cod delimitat de acolade{...}
 class BlockNode : public ASTNode {
 public:
-    vector<ASTNode*> statements;
+    vector<ASTNode*> statements; //lista de instructiuni din bloc
+    
+    //adaugam instructiune doar daca pointerul e valid
     void addStatement(ASTNode* stmt) { if(stmt) statements.push_back(stmt); }
+    
     void print(int level = 0) override {
         ASTNode::print(level); cout << "Block {" << endl;
         for (auto s : statements) s->print(level + 1);
@@ -38,39 +52,49 @@ public:
     }
 };
 
+//declaratie variabila:tip nume(=valoare)
 class VarDeclNode : public ASTNode {
 public:
-    TypeInfo* type;
-    string name;
-    ASTNode* initVal;
+    TypeInfo* type;   //ex:INT,FLOAT
+    string name;      //ex:"myVar"
+    ASTNode* initVal; //ex:5 in "int x=5;",poate fi nullptr
+    
+    //constructor cu valoare de initializare optionala(default null)
     VarDeclNode(TypeInfo* t, string n, ASTNode* init = nullptr) : type(t), name(n), initVal(init) {}
+    
     void print(int level = 0) override {
         ASTNode::print(level); cout << "VarDecl: " << name << endl;
+        //afisam si initializarea doar daca exista
+        /*if(initVal)... logica de afisare copil...*/
     }
 };
 
+//definitie functie:tip nume(params){body}
 class FuncDefNode : public ASTNode {
 public:
     TypeInfo* returnType;
     string name;
-    vector<VarDeclNode*> params;
-    BlockNode* body;
+    vector<VarDeclNode*> params; //lista de parametri formali
+    BlockNode* body;             //corpul functiei
     
     FuncDefNode(TypeInfo* rt, string n, vector<VarDeclNode*> p, BlockNode* b) 
         : returnType(rt), name(n), params(p), body(b) {}
 
     void print(int level = 0) override {
         ASTNode::print(level); cout << "Function: " << name << endl;
+        //afisam numarul de parametri pentru debug
         cout << string(level+2, ' ') << "Params: " << params.size() << endl;
         for(auto p : params) p->print(level + 2);
+        //afisam corpul functiei indentat
         if(body) body->print(level + 1);
     }
 };
 
+//definitie clasa:class Nume{membri}
 class ClassDefNode : public ASTNode {
 public:
     string name;
-    vector<ASTNode*> members;
+    vector<ASTNode*> members; //variabile sau metode
     ClassDefNode(string n) : name(n) {}
     void print(int level = 0) override {
         ASTNode::print(level); cout << "Class: " << name << endl;
@@ -78,6 +102,7 @@ public:
     }
 };
 
+//nod special pentru main()
 class MainNode : public ASTNode {
 public:
     BlockNode* body;
@@ -88,6 +113,7 @@ public:
     }
 };
 
+//statement generic(wrapper)
 class StmtNode : public ASTNode {
 public:
     string info;
@@ -97,6 +123,7 @@ public:
     }
 };
 
+//structura de control if(cond){then}
 class IfNode : public ASTNode {
 public:
     ASTNode* condition;
@@ -109,6 +136,7 @@ public:
     }
 };
 
+//structura repetitiva while(cond){body}
 class WhileNode : public ASTNode {
 public:
     ASTNode* condition;
@@ -121,6 +149,7 @@ public:
     }
 };
 
+//nod pentru Print(expr)
 class PrintNode : public ASTNode {
 public:
     ASTNode* expr;
@@ -131,6 +160,7 @@ public:
     }
 };
 
+//atribuire:id=val
 class AssignNode : public ASTNode {
 public:
     string name;
@@ -142,9 +172,10 @@ public:
     }
 };
 
+//return expr;sau return;
 class ReturnNode : public ASTNode {
 public:
-    ASTNode* expr;
+    ASTNode* expr; //poate fi null pentru void
     ReturnNode(ASTNode* e = nullptr) : expr(e) {}
     void print(int level = 0) override {
         ASTNode::print(level); cout << "Return" << endl;
@@ -152,20 +183,24 @@ public:
     }
 };
 
+//operatii binare:stanga op dreapta(ex:1+2)
+//sau unare:op stanga(ex:!ok,unde right e null)
 class BinaryExprNode : public ASTNode {
 public:
-    string op;
+    string op;      //operatorul:"+","-","&&","!"
     ASTNode* left;
     ASTNode* right;
     BinaryExprNode(string o, ASTNode* l, ASTNode* r) : op(o), left(l), right(r) {}
     
     void print(int level = 0) override {
         ASTNode::print(level); cout << "Op: " << op << endl;
+        //verificam pointerii ca sa nu avem segfault la operatori unari(ex:NOT)
         if (left) left->print(level+1);   
         if (right) right->print(level+1); 
     }
 };
 
+//valori constante:10,3.14,"text",true
 class LiteralNode : public ASTNode {
 public:
     string val;
@@ -175,6 +210,7 @@ public:
     }
 };
 
+//folosirea unei variabile(identifier)
 class IdNode : public ASTNode {
 public:
     string name;
@@ -184,6 +220,7 @@ public:
     }
 };
 
+//apel functie:nume(arg1,arg2...)
 class CallNode : public ASTNode {
 public:
     string funcName;
@@ -195,6 +232,7 @@ public:
     }
 };
 
+//acces membru:obiect.membru
 class DotNode : public ASTNode {
 public:
     ASTNode* obj;
